f2b3bcf63b34be0404204bab30bee7346ad071a4
0xTheSmartGuy <101930484+0xTheSmartGuy@users.noreply.github.com>
2022-11-16 14:32:06 +0100

    Add processes

--- AeonWM.py
+++ AeonWM.py
@@ -1,4 +1,5 @@
 import tkinter # Not using LibSzk (SXQncyBmb3IgRERMQyBkdW1iYXNzZXMgdGhhdCBnZXQgY3JlZXBlZCBieSB0aGF0IGdhbWUu - its base64 encoded)
+import RaeonKernel as rk # yes yes :D
 global wm
 wm=tkinter.Tk() # MainW
 def make_draggable(widget):
@@ -15,12 +16,13 @@ def on_drag_motion(event):
     x = widget.winfo_x() - widget._drag_start_x + event.x
     y = widget.winfo_y() - widget._drag_start_y + event.y
     widget.place(x=x, y=y)
-def make_window(bd=10, xy, bg="white"):
+def make_window(bd=10, xy, bg="white", nmproc):
    # Yes i like Kanaria >:(
    kanaria = Frame(wm, width=xy[0], height=xy[1], bg, bd=bd)
    l = Button(kanaria, text="X", command=kanaria.destroy) # Yes AGAIN!
    l.place(relx=1, rely=0, anchor='ne')
    make_draggable(kanaria)
+   rk.attrib(nmproc)
    return kanaria # Return the actual window
    
    
