c92e141abd319aa296e83513ac6da93e77d761fb
0xTheSmartGuy <101930484+0xTheSmartGuy@users.noreply.github.com>
2022-11-16 07:55:13 +0100

    Put WM and Window

---
+++ AeonWM.py
@@ -0,0 +1,26 @@
+import tkinter # Not using LibSzk (SXQncyBmb3IgRERMQyBkdW1iYXNzZXMgdGhhdCBnZXQgY3JlZXBlZCBieSB0aGF0IGdhbWUu - its base64 encoded)
+global wm
+wm=tkinter.Tk() # MainW
+def make_draggable(widget):
+    widget.bind("<Button-1>", on_drag_start)
+    widget.bind("<B1-Motion>", on_drag_motion)
+
+def on_drag_start(event):
+    widget = event.widget
+    widget._drag_start_x = event.x
+    widget._drag_start_y = event.y
+
+def on_drag_motion(event):
+    widget = event.widget
+    x = widget.winfo_x() - widget._drag_start_x + event.x
+    y = widget.winfo_y() - widget._drag_start_y + event.y
+    widget.place(x=x, y=y)
+def make_window(bd=10, xy, bg="white"):
+   # Yes i like Kanaria >:(
+   kanaria = Frame(wm, width=xy[0], height=xy[1], bg, bd=bd)
+   l = Button(kanaria, text="X", command=kanaria.destroy) # Yes AGAIN!
+   l.place(relx=1, rely=0, anchor='ne')
+   make_draggable(kanaria)
+   return kanaria # Return the actual window
+   
+   
